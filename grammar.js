53ed75c333d88d17fd20ab893859e4cc6150e34f
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-07-10 03:04:40 +0100

    Sketch out declarations

--- grammar.js
+++ grammar.js
@@ -2,6 +2,17 @@ module.exports = grammar({
   name: "rbs",
 
   rules: {
-    program: $ => optional(/\w+/)
+    program: $ => repeat(
+      // $.declaration
+    ),
+
+    // declaration: $ => choice(
+      // $.class_declaration,
+      // $.module_declaration,
+      // $.interface_declaration,
+      // $.type_alias_declaration,
+      // $.constant_declaration,
+      // $.global_declaration
+    // ),
   }
 })
