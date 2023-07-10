ac3b7d3c462410836918388468596206959e660d
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-07-10 03:10:29 +0100

    Implement class_declaration skeleton

--- grammar.js
+++ grammar.js
@@ -3,16 +3,25 @@ module.exports = grammar({
 
   rules: {
     program: $ => repeat(
-      // $.declaration
+      $.declaration
     ),
 
-    // declaration: $ => choice(
-      // $.class_declaration,
+    declaration: $ => choice(
+      $.class_declaration,
       // $.module_declaration,
       // $.interface_declaration,
       // $.type_alias_declaration,
       // $.constant_declaration,
       // $.global_declaration
-    // ),
+    ),
+
+    class_declaration: $ => seq(
+      "class",
+      // $.class_name,
+      // $.module_type_parameters,
+      // < $.class_name $.type_arguments
+      // $.members,
+      "end"
+    )
   }
 })
--- src/parser.c
+++ src/parser.c
@@ -6,31 +6,43 @@
 #endif
 
 #define LANGUAGE_VERSION 14
-#define STATE_COUNT 4
-#define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 3
+#define STATE_COUNT 8
+#define LARGE_STATE_COUNT 4
+#define SYMBOL_COUNT 7
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 2
+#define TOKEN_COUNT 3
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
-#define MAX_ALIAS_SEQUENCE_LENGTH 1
+#define MAX_ALIAS_SEQUENCE_LENGTH 2
 #define PRODUCTION_ID_COUNT 1
 
 enum {
-  aux_sym_program_token1 = 1,
-  sym_program = 2,
+  anon_sym_class = 1,
+  anon_sym_end = 2,
+  sym_program = 3,
+  sym_declaration = 4,
+  sym_class_declaration = 5,
+  aux_sym_program_repeat1 = 6,
 };
 
 static const char * const ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
-  [aux_sym_program_token1] = "program_token1",
+  [anon_sym_class] = "class",
+  [anon_sym_end] = "end",
   [sym_program] = "program",
+  [sym_declaration] = "declaration",
+  [sym_class_declaration] = "class_declaration",
+  [aux_sym_program_repeat1] = "program_repeat1",
 };
 
 static const TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
-  [aux_sym_program_token1] = aux_sym_program_token1,
+  [anon_sym_class] = anon_sym_class,
+  [anon_sym_end] = anon_sym_end,
   [sym_program] = sym_program,
+  [sym_declaration] = sym_declaration,
+  [sym_class_declaration] = sym_class_declaration,
+  [aux_sym_program_repeat1] = aux_sym_program_repeat1,
 };
 
 static const TSSymbolMetadata ts_symbol_metadata[] = {
@@ -38,13 +50,29 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .visible = false,
     .named = true,
   },
-  [aux_sym_program_token1] = {
-    .visible = false,
+  [anon_sym_class] = {
+    .visible = true,
+    .named = false,
+  },
+  [anon_sym_end] = {
+    .visible = true,
     .named = false,
   },
   [sym_program] = {
     .visible = true,
     .named = true,
+  },
+  [sym_declaration] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_class_declaration] = {
+    .visible = true,
+    .named = true,
+  },
+  [aux_sym_program_repeat1] = {
+    .visible = false,
+    .named = false,
   },
 };
 
@@ -61,6 +89,10 @@ static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
   [1] = 1,
   [2] = 2,
   [3] = 3,
+  [4] = 4,
+  [5] = 5,
+  [6] = 6,
+  [7] = 7,
 };
 
 static bool ts_lex(TSLexer *lexer, TSStateId state) {
@@ -68,25 +100,40 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(1);
+      if (eof) ADVANCE(7);
+      if (lookahead == 'c') ADVANCE(3);
+      if (lookahead == 'e') ADVANCE(4);
       if (lookahead == '\t' ||
           lookahead == '\n' ||
           lookahead == '\r' ||
           lookahead == ' ') SKIP(0)
-      if (('0' <= lookahead && lookahead <= '9') ||
-          ('A' <= lookahead && lookahead <= 'Z') ||
-          lookahead == '_' ||
-          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(2);
       END_STATE();
     case 1:
-      ACCEPT_TOKEN(ts_builtin_sym_end);
+      if (lookahead == 'a') ADVANCE(6);
       END_STATE();
     case 2:
-      ACCEPT_TOKEN(aux_sym_program_token1);
-      if (('0' <= lookahead && lookahead <= '9') ||
-          ('A' <= lookahead && lookahead <= 'Z') ||
-          lookahead == '_' ||
-          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(2);
+      if (lookahead == 'd') ADVANCE(9);
+      END_STATE();
+    case 3:
+      if (lookahead == 'l') ADVANCE(1);
+      END_STATE();
+    case 4:
+      if (lookahead == 'n') ADVANCE(2);
+      END_STATE();
+    case 5:
+      if (lookahead == 's') ADVANCE(8);
+      END_STATE();
+    case 6:
+      if (lookahead == 's') ADVANCE(5);
+      END_STATE();
+    case 7:
+      ACCEPT_TOKEN(ts_builtin_sym_end);
+      END_STATE();
+    case 8:
+      ACCEPT_TOKEN(anon_sym_class);
+      END_STATE();
+    case 9:
+      ACCEPT_TOKEN(anon_sym_end);
       END_STATE();
     default:
       return false;
@@ -98,41 +145,78 @@ static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [1] = {.lex_state = 0},
   [2] = {.lex_state = 0},
   [3] = {.lex_state = 0},
+  [4] = {.lex_state = 0},
+  [5] = {.lex_state = 0},
+  [6] = {.lex_state = 0},
+  [7] = {.lex_state = 0},
 };
 
 static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
-    [aux_sym_program_token1] = ACTIONS(1),
+    [anon_sym_class] = ACTIONS(1),
+    [anon_sym_end] = ACTIONS(1),
   },
   [1] = {
-    [sym_program] = STATE(3),
+    [sym_program] = STATE(7),
+    [sym_declaration] = STATE(2),
+    [sym_class_declaration] = STATE(4),
+    [aux_sym_program_repeat1] = STATE(2),
     [ts_builtin_sym_end] = ACTIONS(3),
-    [aux_sym_program_token1] = ACTIONS(5),
+    [anon_sym_class] = ACTIONS(5),
+  },
+  [2] = {
+    [sym_declaration] = STATE(3),
+    [sym_class_declaration] = STATE(4),
+    [aux_sym_program_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(7),
+    [anon_sym_class] = ACTIONS(5),
+  },
+  [3] = {
+    [sym_declaration] = STATE(3),
+    [sym_class_declaration] = STATE(4),
+    [aux_sym_program_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(9),
+    [anon_sym_class] = ACTIONS(11),
   },
 };
 
 static const uint16_t ts_small_parse_table[] = {
   [0] = 1,
-    ACTIONS(7), 1,
+    ACTIONS(14), 2,
       ts_builtin_sym_end,
-  [4] = 1,
-    ACTIONS(9), 1,
+      anon_sym_class,
+  [5] = 1,
+    ACTIONS(16), 2,
+      ts_builtin_sym_end,
+      anon_sym_class,
+  [10] = 1,
+    ACTIONS(18), 1,
+      anon_sym_end,
+  [14] = 1,
+    ACTIONS(20), 1,
       ts_builtin_sym_end,
 };
 
 static const uint32_t ts_small_parse_table_map[] = {
-  [SMALL_STATE(2)] = 0,
-  [SMALL_STATE(3)] = 4,
+  [SMALL_STATE(4)] = 0,
+  [SMALL_STATE(5)] = 5,
+  [SMALL_STATE(6)] = 10,
+  [SMALL_STATE(7)] = 14,
 };
 
 static const TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
   [3] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_program, 0),
-  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
+  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(6),
   [7] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_program, 1),
-  [9] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+  [9] = {.entry = {.count = 1, .reusable = true}}, REDUCE(aux_sym_program_repeat1, 2),
+  [11] = {.entry = {.count = 2, .reusable = true}}, REDUCE(aux_sym_program_repeat1, 2), SHIFT_REPEAT(6),
+  [14] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_declaration, 1),
+  [16] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_class_declaration, 2),
+  [18] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
+  [20] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
 };
 
 #ifdef __cplusplus
