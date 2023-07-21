43626746f74602e9d69ccf6236a5c19c6304c129
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-07-21 18:57:47 +0100

    Add endwise queries

---
+++ queries/endwise.scm
@@ -0,0 +1,3 @@
+(("module"    (module_name)    @cursor) @endable @indent (#endwise! "end"))
+(("class"     (class_name)     @cursor) @endable @indent (#endwise! "end"))
+(("interface" (interface_name) @cursor) @endable @indent (#endwise! "end"))
